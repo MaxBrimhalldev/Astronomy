6aa54931fcb37e05dd78c3205cdb7b943986c361
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-05-25 21:30:38 -0600

    Create main.js
    API format sent in

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// API Collection
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://www.dnd5eapi.co/api/equipment/`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
