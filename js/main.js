6ca6a08b84b0252713fa020890773a4f068cd2b9
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-05-27 12:06:45 -0600

    Added api
    Simple pic of stars

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
   const url = 'https://api.nasa.gov/planetary/apod?api_key=X1xQxpVTOwpDJUllNfZ4INksYfkm8RJe3lZDUMkd'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+		document.querySelector('img').src = data.hdurl
+		document.querySelector('#description').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
