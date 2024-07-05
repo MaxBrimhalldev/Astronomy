ba6c63648ca50e6f3f1540dca005bc7cddac9882
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-07-05 16:05:39 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,3 +82,26 @@
 		}
 
 })(jQuery);
+
+// API Collection
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=X1xQxpVTOwpDJUllNfZ4INksYfkm8RJe3lZDUMkd'
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+		if(data.media_type === 'image'){
+			document.querySelector('img').src = data.hdurl
+		}else{
+			document.querySelector('iframe').src = data.url
+		}
+		
+		document.querySelector('#description').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
